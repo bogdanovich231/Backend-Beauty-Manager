2fdceb874b688123a9ff9b7220b7db0205b3aead
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-03-09 20:43:03 +0100

    feat: create function for add  user in dynamodb

--- manager-service/lib/manager-service-stack.ts
+++ manager-service/lib/manager-service-stack.ts
@@ -1,8 +1,58 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class ManagerServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const usersTable = dynamodb.Table.fromTableName(
+      this,
+      "UsersTable",
+      "Users"
+    );
+
+    const lambdaRole = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+
+    usersTable.grantReadWriteData(lambdaRole);
+
+    const createUserLambda = new lambda.Function(this, "CreateUserLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "createUser.createUser",
+      code: lambda.Code.fromAsset("dist/handlers"),
+      environment: {
+        USERS_TABLE: usersTable.tableName,
+      },
+      role: lambdaRole,
+    });
+
+    const api = new apigateway.RestApi(this, "ManagerServiceApi", {
+      restApiName: "Manager Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
+    });
+
+    const usersResource = api.root.addResource("users");
+    usersResource.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(createUserLambda)
+    );
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url!,
+    });
   }
 }
